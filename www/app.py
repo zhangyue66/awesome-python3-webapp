27cb3577987d6d5a58b323f5a8d6290df62a39f7
Yue Zhang <zhangyue19890116@gmail.com>
2021-01-28 15:52:54 -0800

    0.2 beta

--- www/app.py
+++ www/app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
+    #download failure
     return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
 
 #@asyncio.coroutine gone from 3.7
@@ -14,8 +15,8 @@ async def init(loop):
     app.router.add_route('GET', '/', index)
     apprunner = web.AppRunner(app)
     await apprunner.setup()
-    srv = await loop.create_server(apprunner.server, '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    srv = await loop.create_server(apprunner.server, '127.0.0.1', 80)
+    logging.info('server started at http://127.0.0.1:80...')
     return srv
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
